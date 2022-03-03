b56e2811617b599d10f54a02bb6b1b03edc82082
Jakub Kotek <blaztar@gmail.com>
2022-03-03 09:04:13 +0100

    clean up unused edges and calculatePath props and utils

--- src/util.ts
+++ src/util.ts
@@ -9,10 +9,6 @@ export class Vector2 {
     return Math.hypot(that.x - this.x, that.y - this.y);
   }
 
-  toString() {
-    return `[${this.x.toFixed(2)}, ${this.y.toFixed(2)}]`;
-  }
-
   css() {
     return { left: `${this.x}px`, top: `${this.y}px` };
   }
@@ -27,14 +23,6 @@ export class Vector2 {
 export const v2 = (x: number = 0, y: number = 0) => new Vector2(x, y);
 v2.from = (v: [number, number] | MouseEvent) =>
   v instanceof MouseEvent ? new Vector2(v.clientX, v.clientY) : new Vector2(v[0], v[1]);
-/**
- * Calculates the midpoint of `a` and `b`
- */
-v2.pmid = (a: Vector2, b: Vector2) => new Vector2((a.x + b.x) / 2, (a.y + b.y) / 2);
-/**
- * Calculates the midpoint of `rect`
- */
-v2.rectMid = (rect: DOMRect) => new Vector2(rect.x + rect.width / 2, rect.y + rect.height / 2);
 v2.offsetOf = (rect: DOMRect) => new Vector2(rect.left, rect.top);
 
 /**
@@ -55,30 +43,6 @@ export const classes = (...v: (string | [string, boolean])[]) =>
     })
     .filter((v) => v !== null)
     .join(" ");
-
-/**
- * SVG path generators
- *
- * Usage:
- * ```jsx
- * <path d={path.line(v2(0, 0), v2(50, 50))} />
- * <path d={path.bezier(v2(0, 0), v2(50, 50))} />
- * ```
- */
-export const path = {
-  line: (start: { x: number; y: number }, end: { x: number; y: number }) =>
-    `M ${start.x} ${start.y} L ${end.x} ${end.y}`,
-  bezier: (start: { x: number; y: number }, end: { x: number; y: number }) => {
-    const mid = v2.pmid(v2(start.x, start.y), v2(end.x, end.y));
-    const dx = end.x - start.x;
-    const dy = end.y - start.y;
-    return (
-      `M ${start.x} ${start.y} ` +
-      `Q ${start.x - dx / 32} ${start.y + dy / 2}, ${mid.x} ${mid.y} ` +
-      `T ${end.x} ${end.y}`
-    );
-  }
-};
 
 /**
  * Remove from `array` all items for which `predicate` is `true`,
@@ -116,47 +80,6 @@ export const removeAll = <T>(
   return removed;
 };
 
-const EDGE_REGEX = /(.+\..+)->(.+\..+)/;
-
-export interface ProcessedEdge {
-  edge: string;
-  d: string;
-}
-export const processEdge = (
-  container: ParentNode,
-  edge: string,
-  offset: Vector2,
-  calculatePath: (
-    from: { x: number; y: number },
-    to: { x: number; y: number }
-  ) => string = path.bezier
-): ProcessedEdge => {
-  const info = edge.match(EDGE_REGEX);
-  if (!info) {
-    throw new Error(
-      `flow-builder: Invalid edge '${edge}', the format should be \`\${source}.\${output}->\${destination}.\${input}`
-    );
-  }
-
-  const [src, dst] = info.slice(1);
-  const from = container.querySelector(`div[data-name='${src}']`);
-  const to = container.querySelector(`div[data-name='${dst}']`);
-  if (!from || !to) {
-    const [which, io] = from ? ["destination", "input"] : ["source", "output"];
-    throw new Error(
-      `flow-builder: Invalid edge '${edge}', ${which} node does not exist or has no such ${io}`
-    );
-  }
-  if (from.parentElement!.dataset.type === "group" || to.parentElement!.dataset.type === "group") {
-    const which = from.parentElement!.dataset.type === "group" ? "source" : "destination";
-    throw new Error(`flow-builder: Invalid edge '${edge}', ${which} node is inside a group`);
-  }
-
-  const srcMid = v2.rectMid(from.getBoundingClientRect()).subtract(offset);
-  const dstMid = v2.rectMid(to.getBoundingClientRect()).subtract(offset);
-  return { edge, d: calculatePath(srcMid, dstMid) };
-};
-
 export function* walkParents(start: HTMLElement) {
   let parent = start.parentElement;
   while (parent) {
@@ -188,5 +111,3 @@ export function multiref<T extends Element>(
     }
   };
 }
-
-export const NOOP = function NOOP() {};
